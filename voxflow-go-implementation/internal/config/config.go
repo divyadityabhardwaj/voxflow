aa251fadfa59f834526086018cdb47e8578f0a62
divyadityabhardwaj <daditya.boss@gmail.com>
2026-03-12 10:52:46 +0530

    fix: set config file permissions to 0600

--- voxflow-go-implementation/internal/config/config.go
+++ voxflow-go-implementation/internal/config/config.go
@@ -167,7 +167,7 @@ func (c *Config) Save() error {
 		return err
 	}
 
-	return os.WriteFile(configPath, data, 0644)
+	return os.WriteFile(configPath, data, 0600)
 }
 
 // GetGeminiAPIKey returns the Gemini API key
